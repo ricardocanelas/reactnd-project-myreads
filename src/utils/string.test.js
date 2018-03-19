497c12220da69edc2773d6a895200e4f05804730
ricardocanelas <ricardocanelas@gmail.com>
2018-03-19 19:07:43 +0100

    Updates

--- src/utils/string.test.js
+++
@@ -1,9 +0,0 @@
-import { removeWhiteSpace } from './string'
-
-it('Remove whitespaces', () => {
-    let str = removeWhiteSpace("   myReads   ")
-    expect('myReads').toBe(str)
-
-    str = removeWhiteSpace("  hello    world   ")
-    expect('hello world').toBe(str)
-})
